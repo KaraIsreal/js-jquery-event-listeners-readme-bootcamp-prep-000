79ab2b14c85f6bd11ee24029512206cc4750eb7b
Kara Isreal <isreal.kara92@gmail.com>
2018-10-13 21:09:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,26 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(key){
+  alert("Hey!")
+});
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+  $(this).addClass('tasty')
+});
+}
+function pressIt(){
+  $(document).on('keydown', function(input){
+    if (key.which == 71){
+      alert('g was pressed');
+}
+}
+});
+
+
+
+
 
 $(document).ready(function(){
 
